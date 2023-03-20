189e0242be0057c2423ab138d9ffb87c26237dd2
Miq De Asis <deasismtv@gmail.com>
2023-03-20 15:44:34 +0800

    feat: added pokemon api model - added getPokemonList

--- lib/utilities/string_constants.dart
+++ lib/utilities/string_constants.dart
@@ -1,3 +1,13 @@
 
 const emptyString = '';
 const homePageText = 'Home Page';
+
+
+const jsonKeyName = 'name';
+const jsonKeyUrl = 'url';
+
+const queryParamLimit = 'limit';
+const queryParamOffset = 'offset';
+
+const endPointPokemon = '/pokemon';
+const responseDataResult = 'results';
