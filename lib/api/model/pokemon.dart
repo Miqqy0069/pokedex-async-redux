a3317f189a85a33e318f127524f5e4616553764a
Miq De Asis <deasismtv@gmail.com>
2023-03-20 18:10:27 +0800

    feat: added pokemon api model - removed @Default on pokemon freezed model

--- lib/api/model/pokemon.dart
+++ lib/api/model/pokemon.dart
@@ -8,8 +8,8 @@ part 'pokemon.g.dart';
 @freezed
 class Pokemon with _$Pokemon{
   factory Pokemon({
-    @Default(emptyString) @JsonKey(name: jsonKeyName) String name,
-    @Default(emptyString) @JsonKey(name: jsonKeyUrl) String url,
+     @JsonKey(name: jsonKeyName) required String name,
+     @JsonKey(name: jsonKeyUrl) required String url,
   }) = _Pokemon;
 
   factory Pokemon.fromJson(Map<String, dynamic> json) => _$PokemonFromJson(json);
