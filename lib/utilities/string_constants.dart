da6fb22cd0de5652a4052057f99ef9641a5aa733
Miq De Asis <deasismtv@gmail.com>
2023-03-23 11:00:53 +0800

    feat: pokemon details action (pr feedback) - added a TODO on string constants for pokemonName to be removed later

--- lib/utilities/string_constants.dart
+++ lib/utilities/string_constants.dart
@@ -4,4 +4,5 @@ const homePageText = 'Home Page';
 const offset = '0';
 const limit = '151';
 
+//TODO: will be removed later as well (test only for dynamic pokemon name)
 const pokemonDitto = 'ditto';
