597fb8b280573c6281e65201f4ddf4a0f2c7776b
Miq De Asis <deasismtv@gmail.com>
2023-03-23 10:53:21 +0800

    feat: pokemon details action (pr feedback) - added pokemonName(ditto) to string constant - added Todo on dispatch for both getpokemon and getpokemondetails

--- lib/features/home_page.dart
+++ lib/features/home_page.dart
@@ -1,14 +1,17 @@
 import 'package:async_redux/async_redux.dart';
 import 'package:flutter/material.dart';
 import 'package:pokedex_async_redux/state/action/pokemon_actions.dart';
+import 'package:pokedex_async_redux/utilities/string_constants.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    //TODO: will be removed later / only for testing
     StoreProvider.dispatch(context, GetPokemonsAction());
-    StoreProvider.dispatch(context, GetPokemonDetailsAction(pokemonName: 'ditto'));
+    //TODO: will be removed later / only for testing
+    StoreProvider.dispatch(context, GetPokemonDetailsAction(pokemonName: pokemonDitto));
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
--- lib/utilities/string_constants.dart
+++ lib/utilities/string_constants.dart
@@ -3,3 +3,5 @@ const homePageText = 'Home Page';
 
 const offset = '0';
 const limit = '151';
+
+const pokemonDitto = 'ditto';
