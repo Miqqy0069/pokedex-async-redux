6b847721c0781e67cad87068447c606afd7e7988
Miq De Asis <deasismtv@gmail.com>
2023-03-23 18:22:51 +0800

    feat: added pokemon overview -display overview of all pokemon -added names to pokemons -added connector -added overview page -added pokemon VM

---
+++ lib/features/pokemon_overview/pokemon_overview_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex_async_redux/api/model/pokemon.dart';
+import 'package:pokedex_async_redux/api/model/pokemon_details.dart';
+import 'package:pokedex_async_redux/utilities/string_constants.dart';
+
+class PokemonOverviewPage extends StatelessWidget {
+  const PokemonOverviewPage({
+    required this.pokemons,
+    Key? key, required this.pokemonDetails,
+  }) : super(key: key);
+
+  final List<Pokemon> pokemons;
+  final PokemonDetails? pokemonDetails;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pokemon List'),
+      ),
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 1,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+        itemCount: pokemons.length,
+        itemBuilder: (context, index) {
+          final pokemon = pokemons[index];
+          final pokemonIndex = index + 1;
+          return Card(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.network(
+                  '$imageURL$pokemonIndex.png',
+                  height: 150,
+                ),
+                Text(pokemon.name),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/utilities/string_constants.dart
+++ lib/utilities/string_constants.dart
@@ -4,5 +4,4 @@ const homePageText = 'Home Page';
 const offset = '0';
 const limit = '151';
 
-//TODO: will be removed later as well (test only for dynamic pokemon name)
-const pokemonDitto = 'ditto';
+const imageURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/';
